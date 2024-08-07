71642bc77b8cccdb19016683a8e629ec6fa78d4b
nika mdinaradze <mdinaradzenika04@gmail.com>
2024-08-07 12:22:38 +0400

    feat: add create and list endpoints for prompt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import models, schemas
+from src.db import get_db
 
 app = FastAPI(
     title="prompt management API", description="Technical Assignment", version="0.0.0"
 )
 
 
-@app.get("/")
-def root():
-    return {"message": "root url"}
+@app.post("/prompts/", response_model=schemas.PromptView)
+async def create_prompt(prompt: schemas.PromptCreate, db: AsyncSession = Depends(get_db)):
+    db_prompt = models.Prompt(**dict(prompt))
+    db.add(db_prompt)
+    await db.commit()
+    await db.refresh(db_prompt)
+    return db_prompt
+
+
+@app.get("/prompts/", response_model=list[schemas.PromptView])
+async def prompts_list(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(models.Prompt))
+    prompts = result.scalars().all()
+    return prompts
